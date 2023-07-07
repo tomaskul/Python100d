c07d2440c7b8e3b568e9e0f208ae656adc9aadd6
tomaskul <>
2023-07-07 10:30:10 +0100

    Generalize random element function to be reusable, solve easy level

--- Beginner/Day5/gen_password.py
+++ Beginner/Day5/gen_password.py
@@ -5,22 +5,25 @@ letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n',
 numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 symbols = ['!', '#', '$', '%', '&', '(', ')', '*', '+']
 
-def get_n_random_letters(number, letters):
+def get_random_elements_from_collection(number, collection):
     result = []
     while number > 0:
-        result.append(random.choice(letters))
+        result.append(random.choice(collection))
         number -= 1
     
     return ''.join(result)
 
 print("Welcome to the PyPassword Generator!")
 nr_letters= int(input("How many letters would you like in your password?\n")) 
-#nr_symbols = int(input(f"How many symbols would you like?\n"))
-#nr_numbers = int(input(f"How many numbers would you like?\n"))
+nr_symbols = int(input(f"How many symbols would you like?\n"))
+nr_numbers = int(input(f"How many numbers would you like?\n"))
 
 #Eazy Level - Order not randomised:
 #e.g. 4 letter, 2 symbol, 2 number = JduE&!91
-print(get_n_random_letters(nr_letters, letters))
+letters = get_random_elements_from_collection(nr_letters, letters)
+symbols = get_random_elements_from_collection(nr_symbols, symbols)
+numbers = get_random_elements_from_collection(nr_numbers, numbers)
+print(f"{letters}{symbols}{numbers}")
 
 #Hard Level - Order of characters randomised:
 #e.g. 4 letter, 2 symbol, 2 number = g^2jk8&P
