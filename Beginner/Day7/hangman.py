6ba320fd9f31a64957371f490e308bd194848a65
tomaskul <>
2023-07-07 15:33:32 +0100

    Improve hangman UX via ASCII art and more words

--- Beginner/Day7/hangman.py
+++ Beginner/Day7/hangman.py
@@ -1,65 +1,9 @@
 import random
-
-stages = ['''
-  +---+
-  |   |
-  O   |
- /|\  |
- / \  |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
- /    |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|\  |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
- /|   |
-      |
-      |
-=========''', '''
-  +---+
-  |   |
-  O   |
-  |   |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-  O   |
-      |
-      |
-      |
-=========
-''', '''
-  +---+
-  |   |
-      |
-      |
-      |
-      |
-=========
-''']
+import hangman_art
+import hangman_words
 
 blank_char = "_"
-word_list = ["aardvark", "baboon", "camel"]
-chosen_word = random.choice(word_list)
+chosen_word = random.choice(hangman_words.word_list)
 lives = 6
 
 #Testing code
@@ -69,11 +13,19 @@ display = []
 for _ in chosen_word:
     display.append(blank_char)
 
+print(hangman_art.logo)
 print(display)
 
 end_of_game = False
+guess_history = []
 while not end_of_game:
     guess = input("Type a letter in the random word:\t")[0].lower()
+
+    if guess in guess_history:
+        print(f"You've already guessed '{guess}', try again.")
+        continue
+    else:
+        guess_history.append(guess)
 
     is_correct_guess = False
     for position in range(0, len(chosen_word)):
@@ -95,4 +47,4 @@ while not end_of_game:
         end_of_game = True
         print("You've win!")
     
-    print(stages[lives])
+    print(hangman_art.stages[lives])
