a0c6188d59b5285b613f3cd43e30ea0df198819d
tomaskul <>
2023-07-07 15:05:35 +0100

    Step 2&3 of hangman game

--- Beginner/Day7/hangman.py
+++ Beginner/Day7/hangman.py
@@ -1,15 +1,22 @@
 import random
 
-#Step 1 
-
 word_list = ["aardvark", "baboon", "camel"]
-
 chosen_word = random.choice(word_list)
+
+#Testing code
+print(f'Pssst, the solution is {chosen_word}.')
+
+display = []
+for _ in chosen_word:
+    display.append("_")
+
+print(display)
 
 guess = input("Type a letter in the random word:\t")[0].lower()
 
-for letter in chosen_word:
-    if letter == guess:
-        print("Right")
-    else:
-        print("Wrong")
+for position in range(0, len(chosen_word)):
+    if chosen_word[position] == guess:
+        display[position] = guess
+    position += 1
+
+print(display)
