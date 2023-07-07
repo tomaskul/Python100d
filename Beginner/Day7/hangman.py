8e57ee75283aba735491931587571c61329bb6a0
tomaskul <>
2023-07-07 15:14:02 +0100

    Replace custom function with build-in functionality

--- Beginner/Day7/hangman.py
+++ Beginner/Day7/hangman.py
@@ -2,12 +2,6 @@ import random
 
 blank_char = "_"
 
-def has_user_won(display):
-    for item in display:
-        if item == blank_char:
-            return False
-    return True
-
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
@@ -20,7 +14,8 @@ for _ in chosen_word:
 
 print(display)
 
-while not has_user_won(display):
+end_of_game = False
+while not end_of_game:
     guess = input("Type a letter in the random word:\t")[0].lower()
 
     for position in range(0, len(chosen_word)):
@@ -29,10 +24,7 @@ while not has_user_won(display):
         position += 1
 
     print(display)
+    if blank_char not in display:
+        end_of_game = True
 
 print("You've won!")
-
-
-
-
-
