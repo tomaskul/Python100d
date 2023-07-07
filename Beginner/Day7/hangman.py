3f31d0f076003f6a56eb947afc1a2bf253902f61
tomaskul <>
2023-07-07 15:11:15 +0100

    Implement main game loop for hangman

--- Beginner/Day7/hangman.py
+++ Beginner/Day7/hangman.py
@@ -1,5 +1,13 @@
 import random
 
+blank_char = "_"
+
+def has_user_won(display):
+    for item in display:
+        if item == blank_char:
+            return False
+    return True
+
 word_list = ["aardvark", "baboon", "camel"]
 chosen_word = random.choice(word_list)
 
@@ -8,15 +16,23 @@ print(f'Pssst, the solution is {chosen_word}.')
 
 display = []
 for _ in chosen_word:
-    display.append("_")
+    display.append(blank_char)
 
 print(display)
 
-guess = input("Type a letter in the random word:\t")[0].lower()
+while not has_user_won(display):
+    guess = input("Type a letter in the random word:\t")[0].lower()
 
-for position in range(0, len(chosen_word)):
-    if chosen_word[position] == guess:
-        display[position] = guess
-    position += 1
+    for position in range(0, len(chosen_word)):
+        if chosen_word[position] == guess:
+            display[position] = guess
+        position += 1
 
-print(display)
+    print(display)
+
+print("You've won!")
+
+
+
+
+
