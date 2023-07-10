5bf4a7db7127659fcab89c35b3212c632aebacca
tomaskul <>
2023-07-10 11:08:29 +0100

    Basic exercise grading implementation

--- Beginner/Day9/exercise_grading.py
+++ Beginner/Day9/exercise_grading.py
@@ -5,3 +5,29 @@ student_scores = {
     "Draco": 74,
     "Neville": 62,
 }
+
+scoring_criteria = {
+    "Outstanding": range(91, 101),
+    "Exceeds Expectations": range(81, 91),
+    "Acceptable": range(71, 81),
+}
+
+student_grades = {}
+
+def grade_students():
+    for student in student_scores:
+        score = student_scores[student]
+
+        criteria_met = False
+        for criteria in scoring_criteria:
+            score_range = scoring_criteria[criteria]
+            if score in score_range:
+                criteria_met = True
+                student_grades[student] = criteria
+        
+        if not criteria_met:
+            student_grades[student] = "Fail"
+
+
+grade_students()
+print(student_grades)
