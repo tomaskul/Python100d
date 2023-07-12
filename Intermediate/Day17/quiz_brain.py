abbe83b985cef668367c942997d738699f709d13
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-07-12 15:59:03 +0100

    Add basic QuizBrain implementation

---
+++ Intermediate/Day17/quiz_brain.py
@@ -0,0 +1,12 @@
+class QuizBrain:
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+
+    def next_question(self):
+        if self.question_number == len(self.question_list):
+            self.question_number = 0
+        
+        next_q = self.question_list[self.question_number]
+        self.question_number += 1
+        return next_q
