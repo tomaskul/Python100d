9f3cdd54ad065b8531b06534d51b3e3361065a73
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-07-13 14:14:34 +0100

    Add drawManySides function

--- Intermediate/Day18/main.py
+++ Intermediate/Day18/main.py
@@ -1,4 +1,5 @@
 from turtle import Turtle, Screen
+import random
 
 def drawSquare(turtle, distance):
     for _ in range(0, 4):
@@ -14,13 +15,24 @@ def drawDashedLine(turtle, line_distance, dash_length):
         turtle.pendown()
         distance_moved += (float(dash_length) + 5.0)
 
+def drawManySides(turtle):
+    colours = ["cornflower blue", "deep pink", "brown", "olive drab", "moccasin", "dark salmon"]
+    for sides in range(3, 11):
+        total_rotation = 0
+        turtle.pencolor(random.choice(colours))
+        while total_rotation < 360:
+            turtle.forward(100.0)
+            angle = 360 / sides
+            turtle.right(angle=angle)
+            total_rotation += angle
+
 turtle = Turtle()
 turtle.shape("turtle")
 
 
-
 #drawSquare(turtle=turtle, distance=50.0)
 #drawDashedLine(turtle, line_distance=200, dash_length=15)
+#drawManySides(turtle)
 
 screen = Screen()
 screen.exitonclick()
