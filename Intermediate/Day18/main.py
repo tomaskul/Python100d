bcbfe1b95045f580f35c9f7e449c5c662c721a52
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-07-13 14:46:37 +0100

    Add drawRandomWalk function

--- Intermediate/Day18/main.py
+++ Intermediate/Day18/main.py
@@ -6,6 +6,7 @@ def drawSquare(turtle, distance):
         turtle.forward(float(distance))
         turtle.right(float(90))
 
+
 def drawDashedLine(turtle, line_distance, dash_length):
     distance_moved = 0
     while distance_moved <= line_distance:
@@ -14,6 +15,7 @@ def drawDashedLine(turtle, line_distance, dash_length):
         turtle.forward(5.0)
         turtle.pendown()
         distance_moved += (float(dash_length) + 5.0)
+
 
 def drawManySides(turtle):
     colours = ["cornflower blue", "deep pink", "brown", "olive drab", "moccasin", "dark salmon"]
@@ -24,6 +26,22 @@ def drawManySides(turtle):
             turtle.forward(100.0)
             turtle.right(angle)
 
+
+def drawRandomWalk(turtle, step_count):
+    colours = ["cornflower blue", "deep pink", "brown", "olive drab", "moccasin", "dark salmon"]
+    turns = [0.0, 90.0, 180.0, 360.0]
+
+    turtle.hideturtle()
+    turtle.pen(pensize=15)
+    turtle.speed(8)
+
+    for _ in range(step_count):
+        turtle.pencolor(random.choice(colours))
+        turn_angle = random.choice(turns)
+        turtle.right(turn_angle)
+        turtle.forward(25)
+
+
 turtle = Turtle()
 turtle.shape("turtle")
 
@@ -31,6 +49,8 @@ turtle.shape("turtle")
 #drawSquare(turtle=turtle, distance=50.0)
 #drawDashedLine(turtle, line_distance=200, dash_length=15)
 #drawManySides(turtle)
+drawRandomWalk(turtle, 100)
+
 
 screen = Screen()
 screen.exitonclick()
