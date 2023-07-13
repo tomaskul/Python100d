27839b239e0aa859fdcb5d467388189d3aff7259
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-07-13 15:16:46 +0100

    Refactoring main.py to have all drawing challenges be self-contained

--- Intermediate/Day18/main.py
+++ Intermediate/Day18/main.py
@@ -1,13 +1,23 @@
 import turtle as t
 import random
 
-def drawSquare(turtle, distance):
+def drawSquare(side_length):
+    '''Draws a square of length sides.'''
+    turtle = t.Turtle()
+    turtle.shape("turtle")
     for _ in range(0, 4):
-        turtle.forward(float(distance))
+        turtle.forward(float(side_length))
         turtle.right(float(90))
 
+    screen = t.Screen()
+    screen.exitonclick()
 
-def drawDashedLine(turtle, line_distance, dash_length):
+
+def drawDashedLine(line_distance, dash_length):
+    '''Draws a dashed line of specified distance and gap lenghts.'''
+    turtle = t.Turtle()
+    turtle.shape("turtle")
+
     distance_moved = 0
     while distance_moved <= line_distance:
         turtle.forward(float(dash_length))
@@ -16,8 +26,15 @@ def drawDashedLine(turtle, line_distance, dash_length):
         turtle.pendown()
         distance_moved += (float(dash_length) + 5.0)
 
+    screen = t.Screen()
+    screen.exitonclick()
 
-def drawManySides(turtle):
+
+def drawManySides():
+    '''Draws shapes from triangle to dekagon, in different colours, on top of one another.'''
+    turtle = t.Turtle()
+    turtle.shape("turtle")
+
     colours = ["cornflower blue", "deep pink", "brown", "olive drab", "moccasin", "dark salmon"]
     for sides in range(3, 11):
         angle = 360 / sides
@@ -26,38 +43,38 @@ def drawManySides(turtle):
             turtle.forward(100.0)
             turtle.right(angle)
 
+    screen = t.Screen()
+    screen.exitonclick()
+
 def randomRgbColour():
+    '''Returns a tuple of a random rgb colour, i.e., (r, g, b).'''
     r = random.randint(0, 255)
     g = random.randint(0, 255)
     b = random.randint(0, 255)
     return (r, g, b)
 
 
-def drawRandomWalk(turtle, step_count):
-    #colours = ["CornflowerBlue", "DarkOrchid", "IndianRed", "DeepPink", "LightSeaGreen", "olive drab", "moccasin", "dark salmon"]
-    turns = [0.0, 90.0, 180.0, 270.0]
-
+def drawRandomWalk(step_count):
+    '''Draws a random walk over specified number of steps.'''
+    t.colormode(255)
+    turtle = t.Turtle()
     turtle.hideturtle()
     turtle.pen(pensize=15)
     turtle.speed(8)
+
+    turns = [0.0, 90.0, 180.0, 270.0]
 
     for _ in range(step_count):
         turtle.pencolor(randomRgbColour())
         turn_angle = random.choice(turns)
         turtle.setheading(turn_angle)
         turtle.forward(25)
+    
+    screen = t.Screen()
+    screen.exitonclick()
 
 
-turtle = t.Turtle()
-t.colormode(255)
-#turtle.shape("turtle")
-
-
-#drawSquare(turtle=turtle, distance=50.0)
-#drawDashedLine(turtle, line_distance=200, dash_length=15)
-#drawManySides(turtle)
-drawRandomWalk(turtle, 200)
-
-
-screen = t.Screen()
-screen.exitonclick()
+#drawSquare(distance=50.0)
+#drawDashedLine(line_distance=200, dash_length=15)
+#drawManySides()
+#drawRandomWalk(200)
