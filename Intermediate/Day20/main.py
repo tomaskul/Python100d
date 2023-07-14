877d4cddde590c52c37b7bb1ef1a72d40294bd72
tomaskul <>
2023-07-14 17:01:08 +0100

    Use slicing to simplify tail collision detection

--- Intermediate/Day20/main.py
+++ Intermediate/Day20/main.py
@@ -45,10 +45,8 @@ while game_is_on:
         scoreboard.game_over()
 
     # Detect tail collision.
-    for segment in snake.body:
-        if segment == snake.head:
-            pass
-        elif snake.head.distance(segment) < 10:
+    for segment in snake.body[1:]:
+        if snake.head.distance(segment) < 10:
             game_is_on = False
             scoreboard.game_over()
 
