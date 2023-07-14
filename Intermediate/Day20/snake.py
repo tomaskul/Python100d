f35f4322742f3d767b07c49fb86aa67d62dac87c
tomaskul <>
2023-07-14 14:41:35 +0100

    Add snake control restrictions

--- Intermediate/Day20/snake.py
+++ Intermediate/Day20/snake.py
@@ -2,6 +2,11 @@ from turtle import Turtle
 
 MOVE_DISTANCE = 20
 
+UP = 90
+DOWN = 270
+LEFT = 180
+RIGHT = 0
+
 class Snake:
     def __init__(self):
         body = []
@@ -23,13 +28,17 @@ class Snake:
         self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.head.setheading(90)
+        if self.head.heading() != DOWN:
+            self.head.setheading(UP)
 
     def down(self):
-        self.head.setheading(270)
+        if self.head.heading() != UP:
+            self.head.setheading(DOWN)
 
     def left(self):
-        self.head.setheading(180)
+        if self.head.heading() != RIGHT:
+            self.head.setheading(LEFT)
     
     def right(self):
-        self.head.setheading(0)
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
