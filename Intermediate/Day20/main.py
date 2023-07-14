6b2f2ea7b374155d2e338c3194c1f5553123ec28
tomaskul <>
2023-07-14 15:08:01 +0100

    Add escape button binding to end game, add food refresh method

--- Intermediate/Day20/main.py
+++ Intermediate/Day20/main.py
@@ -12,17 +12,29 @@ screen.tracer(0)
 snake = Snake()
 food = Food()
 
+game_is_on = True
+def stop_game():
+    global game_is_on
+    game_is_on = False
+
 screen.listen()
 screen.onkey(snake.up, "Up")
 screen.onkey(snake.down,"Down")
 screen.onkey(snake.left, "Left")
 screen.onkey(snake.right, "Right")
+screen.onkey(stop_game, "Escape")
 
-game_is_on = True
+
 while game_is_on:
     screen.update()
     time.sleep(0.1)
     snake.move()
+
+    # detect collisions
+    if snake.head.distance(food) < 15:
+        #print("nom nom nom..")
+        food.refresh()
+
 
 
 screen.exitonclick()
