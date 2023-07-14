933c9cb88501f2e6ce1bb2a1e019fba09c386100
tomaskul <>
2023-07-14 14:36:59 +0100

    Add snake head reference to simplify code

--- Intermediate/Day20/snake.py
+++ Intermediate/Day20/snake.py
@@ -13,22 +13,23 @@ class Snake:
             body.append(square)
 
         self.body = body
+        self.head = body[0]
 
     def move(self):
         for seg_id in range(len(self.body) -1, 0, -1):
             new_x = self.body[seg_id - 1].xcor()
             new_y = self.body[seg_id - 1].ycor()
             self.body[seg_id].goto(new_x, new_y)
-        self.body[0].forward(MOVE_DISTANCE)
+        self.head.forward(MOVE_DISTANCE)
 
     def up(self):
-        self.body[0].setheading(90)
+        self.head.setheading(90)
 
     def down(self):
-        self.body[0].setheading(270)
+        self.head.setheading(270)
 
     def left(self):
-        self.body[0].setheading(180)
+        self.head.setheading(180)
     
     def right(self):
-        self.body[0].setheading(0)
+        self.head.setheading(0)
