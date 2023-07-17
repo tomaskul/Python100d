c7f3286c4d500631cfe13c207b74c366cd331785
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-07-17 14:49:12 +0100

    Add starting_letter.txt read

--- Intermediate/Day24/main.py
+++ Intermediate/Day24/main.py
@@ -2,6 +2,12 @@
 #for each name in invited_names.txt
 #Replace the [name] placeholder with the actual name.
 #Save the letters in the folder "ReadyToSend".
+
+starting_letter = ""
+with open("Intermediate/Day24/Input/Letters/starting_letter.txt") as f:
+    starting_letter = f.read()
+
+print(f"Starting letter:\n\n{starting_letter}")
     
 #Hint1: This method will help you: https://www.w3schools.com/python/ref_file_readlines.asp
     #Hint2: This method will also help you: https://www.w3schools.com/python/ref_string_replace.asp
