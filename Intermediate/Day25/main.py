8235e7ce7173912fc347d1b2869824f8ebdce766
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-07-18 12:20:31 +0100

    Adding game score

--- Intermediate/Day25/main.py
+++ Intermediate/Day25/main.py
@@ -10,17 +10,24 @@ states = pandas.read_csv("Intermediate/Day25/50_states.csv")
 
 game_is_on = True
 tries = 3
+score = 0
+answered_state_names = []
 while game_is_on:
-    answer_state = screen.textinput(title="Guess the state", prompt="What's another state's name?")
+    answer_state = screen.textinput(title=f"Guess the state (Score: {score}) (Remaining tries: {tries})", prompt="What's another state's name?")
     if len(states[states["state"] == answer_state]) > 0:
-        state = states[states["state"] == answer_state].iloc[0]
-        #print(state)
+        if answer_state in answered_state_names:
+            pass
+        else:
+            answered_state_names.append(answer_state)
+            state = states[states["state"] == answer_state].iloc[0]
 
-        correct_answer = turtle.Turtle(visible=False)
-        correct_answer.penup()
-        correct_answer.speed("fastest")
-        correct_answer.goto(float(state.x), float(state.y))
-        correct_answer.write(arg=str(state.state), font=("Arial", 8, "normal"))
+            correct_answer = turtle.Turtle(visible=False)
+            correct_answer.penup()
+            correct_answer.speed("fastest")
+            correct_answer.goto(float(state.x), float(state.y))
+            correct_answer.write(arg=state.state, font=("Arial", 8, "normal"))
+
+            score += 1
     else:
         tries -= 1
         if tries == 0:
