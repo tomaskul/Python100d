c7076bf65a1cd7eeb378c3a2723f4c47c759d13d
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-07-18 15:31:20 +0100

    Bug fix, all characters are translated

--- Intermediate/Day26/main.py
+++ Intermediate/Day26/main.py
@@ -6,7 +6,7 @@ alphabet_df = pandas.read_csv("Intermediate/Day26/nato_phonetic_alphabet.csv")
 alphabet_dictionary = {row.letter:row.code for (_,row) in alphabet_df.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-input_text = input("Enter word to be spelt in NATO phonetic alphabet:\t").capitalize()
+input_text = input("Enter word to be spelt in NATO phonetic alphabet:\t").upper()
 output_text = [alphabet_dictionary[character] for character in input_text if character in alphabet_dictionary.keys()]
 print(output_text)
 
