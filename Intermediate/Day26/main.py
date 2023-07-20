8c0aba10f8d29fe9c76958575d639fb12958cbf3
Tomas <16058999+tomaskul@users.noreply.github.com>
2023-07-20 15:41:24 +0100

    Day 30 - Add exception handling (#13)
    * Remove todos
    
    * Add try-except block as per Day 30 challenge. Not previously required due to if-statement within dictionary comprehension.

--- Intermediate/Day26/main.py
+++ Intermediate/Day26/main.py
@@ -1,12 +1,14 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
 alphabet_df = pandas.read_csv("Intermediate/Day26/nato_phonetic_alphabet.csv")
 alphabet_dictionary = {row.letter:row.code for (_,row) in alphabet_df.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
-input_text = input("Enter word to be spelt in NATO phonetic alphabet:\t").upper()
-output_text = [alphabet_dictionary[character] for character in input_text if character in alphabet_dictionary.keys()]
-print(output_text)
-
+while True:
+    try:
+        input_text = input("Enter word to be spelt in NATO phonetic alphabet:\t").upper()
+        # Alternatively to try-except, use: 'if character in alphabet_dictionary.keys()' within dictionary comprehension.
+        output_text = [alphabet_dictionary[character] for character in input_text]
+        print(output_text)
+        break
+    except KeyError:
+        print("Sorry, only letters in the English alphabet please.")
