f9e3e738246cb7c84b64752b4d96bd8ebe171ed6
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-07-20 15:30:02 +0100

    Remove todos

--- Intermediate/Day26/main.py
+++ Intermediate/Day26/main.py
@@ -1,11 +1,8 @@
 import pandas
 
-#TODO 1. Create a dictionary in this format:
-{"A": "Alfa", "B": "Bravo"}
 alphabet_df = pandas.read_csv("Intermediate/Day26/nato_phonetic_alphabet.csv")
 alphabet_dictionary = {row.letter:row.code for (_,row) in alphabet_df.iterrows()}
 
-#TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 input_text = input("Enter word to be spelt in NATO phonetic alphabet:\t").upper()
 output_text = [alphabet_dictionary[character] for character in input_text if character in alphabet_dictionary.keys()]
 print(output_text)
