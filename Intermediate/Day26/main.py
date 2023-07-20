72a82816cf0c7fa587ecf8c8a97b13f41a1ebc80
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-07-20 15:38:39 +0100

    Add try-except block as per Day 30 challenge. Not previously required due to if-statement within dictionary comprehension.

--- Intermediate/Day26/main.py
+++ Intermediate/Day26/main.py
@@ -3,7 +3,12 @@ import pandas
 alphabet_df = pandas.read_csv("Intermediate/Day26/nato_phonetic_alphabet.csv")
 alphabet_dictionary = {row.letter:row.code for (_,row) in alphabet_df.iterrows()}
 
-input_text = input("Enter word to be spelt in NATO phonetic alphabet:\t").upper()
-output_text = [alphabet_dictionary[character] for character in input_text if character in alphabet_dictionary.keys()]
-print(output_text)
-
+while True:
+    try:
+        input_text = input("Enter word to be spelt in NATO phonetic alphabet:\t").upper()
+        # Alternatively to try-except, use: 'if character in alphabet_dictionary.keys()' within dictionary comprehension.
+        output_text = [alphabet_dictionary[character] for character in input_text]
+        print(output_text)
+        break
+    except KeyError:
+        print("Sorry, only letters in the English alphabet please.")
