6c69285332759363001a1cf109f5749d2c90c59e
tomaskul <16058999+tomaskul@users.noreply.github.com>
2023-08-04 11:01:53 +0100

    add flip_card function on initial delay

--- Intermediate/Day31/main.py
+++ Intermediate/Day31/main.py
@@ -5,9 +5,32 @@ import time
 WINDOW_WIDTH = 800
 WINDOW_HEIGHT = 526
 BACKGROUND_COLOR = "#B1DDC6"
+TITLE_FONT = ("Arial", 40, "italic")
+WORD_FONT = ("Arial", 60, "bold")
+FRONT_TEXT_COLOR = "black"
+BACK_TEXT_COLOR = "white"
 
 word_gen = wg.WordGenerator()
-word = word_gen.nextWord()
+current_word = word_gen.nextWord()
+current_language = wg.SPANISH
+
+def flip_card():
+    # Remove current content & flip the card.
+    text_color = ""
+    global current_language
+    if current_language == wg.ENGLISH:
+        # Flip to front - Spanish.
+        current_language = wg.SPANISH
+        canvas.itemconfig(canvas_image, image=card_front_img)
+        text_color = BACK_TEXT_COLOR
+    elif current_language == wg.SPANISH:
+        # Flip to back - English.
+        current_language = wg.ENGLISH
+        canvas.itemconfig(canvas_image, image=card_back_img)
+        text_color = FRONT_TEXT_COLOR
+
+    canvas.itemconfig(card_title, text=current_language, fill=text_color)
+    canvas.itemconfig(card_word, text=current_word[current_language], fill=text_color)
 
 
 # ---   --- UI Setup ---    ---
@@ -15,23 +38,23 @@ window = Tk()
 window.title("Spanish-English Flash card game")
 window.config(padx=50, pady=50, bg=BACKGROUND_COLOR)
 
-canvas = Canvas(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+window.after(3000, func=flip_card)
 
+canvas = Canvas(width=WINDOW_WIDTH, height=WINDOW_HEIGHT)
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
 card_back_img = PhotoImage(file="Intermediate/Day31/images/card_back.png")
 card_front_img = PhotoImage(file="Intermediate/Day31/images/card_front.png")
-canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
-canvas.create_image(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, image=card_front_img)
-canvas.create_text(WINDOW_WIDTH/2, 150, text=wg.SPANISH, font=("Arial", 40, "italic"), tags=("title"))
-canvas.create_text(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, text=word[wg.SPANISH], font=("Arial", 60, "bold"), tags=("word"))
+canvas_image = canvas.create_image(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, image=card_front_img)
+card_title = canvas.create_text(WINDOW_WIDTH/2, 150, text=current_language, font=TITLE_FONT, fill=FRONT_TEXT_COLOR)
+card_word = canvas.create_text(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, text=current_word[current_language], font=WORD_FONT, fill=FRONT_TEXT_COLOR)
 canvas.grid(row=0, column=0, columnspan=2)
 
 def update_word():
-    canvas.delete("word")
-    word = word_gen.nextWord()
-    canvas.create_text(WINDOW_WIDTH/2, WINDOW_HEIGHT/2, text=word[wg.SPANISH], font=("Arial", 60, "bold"), tags=("word"))
+    global current_word
+    current_word = word_gen.nextWord()
 
 wrong_answer_img = PhotoImage(file="Intermediate/Day31/images/wrong.png")
-wrong_btn = Button(image=wrong_answer_img, highlightthickness=0, command=update_word)
+wrong_btn = Button(image=wrong_answer_img, highlightthickness=0, command=flip_card)
 wrong_btn.grid(row=1, column=0)
 
 correct_answer_img = PhotoImage(file="Intermediate/Day31/images/right.png")
