3fc2787225323b3d1115b462ac1475738d0789a3
tomaskul <>
2023-10-14 15:11:04 +0100

    Add logic for finding whose birthday it is today

--- Intermediate/Day32/main.py
+++ Intermediate/Day32/main.py
@@ -5,5 +5,10 @@ data = pandas.read_csv("Intermediate/Day32/friends.csv").to_dict(orient="records
 
 friend_birthdays = {friend["Name"]:dt.date.fromisoformat(friend["Dob"]) for friend in data}
 
-print(friend_birthdays)
-print(friend_birthdays["Alice"])
+today = dt.datetime.now()
+friends_to_notify = []
+for (name, dob) in friend_birthdays.items():
+    if dob.month == today.month and dob.day == today.day:
+        friends_to_notify.append(name)
+
+# Wish happy birthday here.
